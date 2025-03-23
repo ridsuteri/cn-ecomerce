1ac5e1a1071199513003b3e91d6ce113c0669658
Riddhi Suteri <ridsuteri@gmail.com>
2025-03-23 11:22:26 +0530

    fix redis connection

--- config/redis.js
+++ config/redis.js
@@ -1,21 +1,24 @@
 const redis = require("redis");
 
 // Create a Redis client
-try {
-  const client = redis.createClient({
-    url: process.env.REDIS_URL,
-  });
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
 
-  // Test the connection
-  client.on("connect", () => {
-    console.log("Connected to Redis server");
-  });
+client.on("connect", () => {
+  console.log("Connected to Redis server");
+});
 
-  client.on("error", (err) => {
-    console.log(`Error connecting to Redis server : ${err}`);
-  });
-} catch (err) {
-  console.log("error with redis configuration", err);
-}
+client.on("error", (err) => {
+  console.log(`Error connecting to Redis server: ${err}`);
+});
 
+(async () => {
+  try {
+    await client.connect();
+  } catch (err) {
+    console.error("Error connecting to Redis:", err);
+  }
+})();
 
+module.exports = client;
